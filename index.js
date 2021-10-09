66abdede36d80ec52d06161a81dbd873f0396c84
hamzaig <hamzaig@yahoo.com>
2021-10-09 19:14:21 +0500

    Reading and Writing Files

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const hello = "Hello World";
-console.log(hello)
+const fs = require("fs");
+
+const inputText = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(inputText);
+// console.log(typeof inputText);
+
+const outputText = `the is read from input.txt file: ${inputText}\nand Write it...`
+fs.writeFileSync("./txt/output.txt", outputText)
+console.log("Text Written");
