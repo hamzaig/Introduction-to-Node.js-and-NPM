3eef8ab1e031fdd42deecf991435bd18c0fbce79
hamzaig <hamzaig@yahoo.com>
2021-10-10 01:21:08 +0500

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http")
+
+//////////////////////////////////////////////
+// Files
 
 // const inputText = fs.readFileSync('./txt/input.txt', 'utf-8');
 // // console.log(inputText);
@@ -8,15 +12,28 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", outputText)
 // console.log("Text Written");
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-            fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", err => {
-                console.log(err);
-            })
-        });
-    });
-});
-console.log("will Output The Data");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//             fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", err => {
+//                 console.log(err);
+//             })
+//         });
+//     });
+// });
+// console.log("will Output The Data");
 // 1st output this and second file data
 
+/////////////////////////////////////////////////////////
+// Http Request
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    // console.log(res);
+    res.end("Hello From the server")
+})
+
+
+server.listen(8000, "localhost", () => {
+    console.log("Listing to requests on port 8000");
+})
+// console.log(server);
