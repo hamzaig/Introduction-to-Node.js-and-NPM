e601612fbbce60665125a2bfd82e945b7a8ca5b3
hamzaig <hamzaig@yahoo.com>
2021-10-14 19:26:19 +0500

    HTML Templating Filling the Templates

--- index.js
+++ index.js
@@ -25,18 +25,41 @@ const http = require("http")
 // 1st output this and second file data
 
 /////////////////////////////////////////////////////////
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        console.log("Hello");
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    }
+    return output;
+}
+
+
 // Http Server
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`).toString();
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`).toString();
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`).toString();
 const dataObj = JSON.parse(data);
 
+// console.log(templateCard);
 
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    // console.log(res);
     const { url } = req;
     // console.log(url);
     if (url === "/" || url === "/overview") {
-        res.end("This is The Overview Page")
+        res.writeHead(200, { "Content-type": "text/html" });
+        const cardHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join("");
+        const output = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardHtml);
+        res.end(output)
+
     } else if (url === "/product") {
         res.end("This is product Page")
     } else if (url === "/api") {
